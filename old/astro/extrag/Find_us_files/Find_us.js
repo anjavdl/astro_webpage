43d7a9ddc6ec0f695d20c702e4b4b887d23b5224
Michael Zingale <michael.zingale@stonybrook.edu>
2016-08-23 17:40:36 -0400

    more work

---
+++ old/astro/extrag/Find_us_files/Find_us.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100211
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Find_us_files/Find_usMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ old/astro/extrag/Group_files/Group.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20100211
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,207),url:'Group_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Group_files/stroke_1.png'},{rect:new IWRect(1,-1,275,2),url:'Group_files/stroke_2.png'},{rect:new IWRect(276,-1,2,2),url:'Group_files/stroke_3.png'},{rect:new IWRect(276,1,2,207),url:'Group_files/stroke_4.png'},{rect:new IWRect(276,208,2,3),url:'Group_files/stroke_5.png'},{rect:new IWRect(1,208,275,3),url:'Group_files/stroke_6.png'},{rect:new IWRect(-1,208,2,3),url:'Group_files/stroke_7.png'}],new IWSize(277,209)),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),stroke_1:new IWStrokeParts([{rect:new IWRect(-1,1,2,191),url:'Group_files/stroke_8.png'},{rect:new IWRect(-1,-1,2,2),url:'Group_files/stroke_9.png'},{rect:new IWRect(1,-1,254,2),url:'Group_files/stroke_10.png'},{rect:new IWRect(255,-1,2,2),url:'Group_files/stroke_11.png'},{rect:new IWRect(255,1,2,191),url:'Group_files/stroke_12.png'},{rect:new IWRect(255,192,2,3),url:'Group_files/stroke_13.png'},{rect:new IWRect(1,192,254,3),url:'Group_files/stroke_14.png'},{rect:new IWRect(-1,192,2,3),url:'Group_files/stroke_15.png'}],new IWSize(256,193))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Group_files/GroupMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
